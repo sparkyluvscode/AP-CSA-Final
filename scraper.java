070ceb8042b9bab71a97676244883ab397d4d4de
Pranil Raichura <pranil.raichura@gmail.com>
2025-12-08 11:40:06 -0800

    decided on project idea and connected to wikipedia

--- scraper.java
+++ scraper.java
@@ -1,13 +1,23 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class scraper {
     
     public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
         //setting up the webscraper
+        System.out.print("Enter the URL to scrape: ");
+        String url = input.nextLine();
+        webscraper(url);
+    
+    }
+
+    public static void webscraper(String url){
         try {
-            Document doc = Jsoup.connect("https://www.google.com").get();
+            Document doc = Jsoup.connect(url).get();
             System.out.println("Successfully connected! Page title: " + doc.title());
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
