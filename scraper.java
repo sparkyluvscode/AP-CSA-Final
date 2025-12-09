53995de8289b7f5847721b12e2f605445a983fa6
Pranil Raichura <pranil.raichura@gmail.com>
2025-12-09 10:59:57 -0800

    extracted all href links from wikipedia

--- scraper.java
+++ scraper.java
@@ -2,6 +2,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
 import java.util.Scanner;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
 public class scraper {
     
@@ -16,9 +18,23 @@ public class scraper {
     }
 
     public static void webscraper(String url){
+        
         try {
+            
+
+            
+            System.out.println("Connecting to " + url);
             Document doc = Jsoup.connect(url).get();
             System.out.println("Successfully connected! Page title: " + doc.title());
+
+            Elements links = doc.select("a[href^='/wiki/']:not([href*=':'])");
+            System.out.println("Links: " + links.size());
+            for (Element link: links){
+                String href = link.attr("href");
+                System.out.println(href);
+            }
+
+
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
